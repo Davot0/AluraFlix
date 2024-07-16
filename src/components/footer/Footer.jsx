b4c6d70564b77ffdd244d18a3c66a9f997c96201
David Leonardo Marin <160354645+Davot0@users.noreply.github.com>
2024-07-15 20:17:42 -0500

    Proyecto AluraFlix

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,56 @@
+import { styled } from 'styled-components';
+import { BsInstagram, BsLinkedin } from "react-icons/bs";
+import { FaSquareFacebook, FaXTwitter } from "react-icons/fa6";
+import './Footer.css';
+import logo from '../../assets/logo.png';
+
+const StyledFooter = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: var(--color-black);
+    padding: 2rem;
+    box-sizing: border-box;
+    border-top: 4px solid var(--color-blue);
+    box-shadow: 0 -10px 15px 0 var(--color-button-shadow);
+`;
+
+const LogoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+        width: clamp(30%, 100%, 10.528rem); 
+        height: auto;
+        background-color: var(--color-black);
+    }
+`;
+
+const FooterText = styled.p`
+    background-color: transparent;
+    font-size: 13px;
+    color: var(--color-white-smoke);
+    margin: 0;
+    line-height: 1.4;
+    text-align: center;
+`;
+
+function Footer() {
+    return (
+        <StyledFooter className='container'>
+            <LogoContainer className='logo'>
+                <img src={logo} alt="Logo" />
+            </LogoContainer>
+
+            <FooterText className='text'>
+                Diseñado por Alura.<br />
+                Desarrollado por David Leonardo Marin Rodriguez<br />
+                2024
+            </FooterText>
+        </StyledFooter>
+    );
+}
+
+export default Footer
